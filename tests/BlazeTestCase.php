d73482a7cd116fe53a8f4f4db97694dfc81ebbed
John Koster <john@stillat.com>
2026-02-26 22:50:38 -0600

    Revert "Update BlazeTestCase.php"
    This reverts commit 450939da8be31f76ab06196fd781a0a3a1d9a190.

--- tests/BlazeTestCase.php
+++ tests/BlazeTestCase.php
@@ -5,22 +5,23 @@ declare(strict_types=1);
 namespace Forte\Tests;
 
 use Forte\Enclaves\EnclavesManager;
-use Forte\ServiceProvider;
 use Livewire\Blaze\BlazeServiceProvider;
-use Orchestra\Testbench\TestCase;
 
-class BlazeTestCase extends TestCase
+class BlazeTestCase extends ForteTestCase
 {
     protected function getPackageProviders($app)
     {
-        return [
-            ServiceProvider::class,
-            BlazeServiceProvider::class,
-        ];
+        $providers = parent::getPackageProviders($app);
+
+        $providers[] = BlazeServiceProvider::class;
+
+        return $providers;
     }
 
     protected function getEnvironmentSetUp($app)
     {
+        parent::getEnvironmentSetUp($app);
+
         /** @var EnclavesManager $reg */
         $reg = $app->make(EnclavesManager::class);
         $reg->defaultEnclave()
